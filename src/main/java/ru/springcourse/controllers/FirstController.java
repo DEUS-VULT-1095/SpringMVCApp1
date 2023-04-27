26050b0281fdc17c9fe2fac80840870c108f6d63
Denis Rogov <kpotuw@gmail.com>
2023-04-28 01:19:02 +0300

    lesson 20 homework

--- src/main/java/ru/springcourse/controllers/FirstController.java
+++ src/main/java/ru/springcourse/controllers/FirstController.java
@@ -2,6 +2,7 @@ package ru.springcourse.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,9 +13,11 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
-        System.out.println("Hello, " + name + " " + surname);
+        //System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
 
         return "first/hello";
     }
@@ -22,5 +25,38 @@ public class FirstController {
     @GetMapping("/goodbye")
     public String goodbyePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculatorPage(@RequestParam(value = "a") double number1,
+                                 @RequestParam(value = "b") double number2,
+                                 @RequestParam(value = "action") String action,
+                                 Model model) {
+        double result = 0d;
+
+        switch (action) {
+            case "multiplication" :
+                result = number1 * number2;
+                break;
+            case "addition":
+                result = number1 + number2;
+                break;
+            case "subtraction":
+                result = number1 - number2;
+                break;
+            case "division":
+                result = number1 / number2;
+                break;
+            default:
+
+                return "first/calculatorIncorrect";
+        }
+
+        model.addAttribute("number1", number1);
+        model.addAttribute("number2", number2);
+        model.addAttribute("action", action);
+        model.addAttribute("result", result);
+
+        return "first/calculator";
     }
 }
